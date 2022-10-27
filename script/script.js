a0b9f01ec72535825f3b126a8ba9167d5ccb441f
Andrey Ilin <black-hacker@inbox.ru>
2022-10-27 11:23:52 +0300

    Freelance shavuxa

---
+++ script/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    const customer = document.getElementById('customer');
+    const freelancer = document.getElementById('freelancer');
+
+    console.log(customer, freelancer);
+
+    customer.addEventListener('click', () => {
+
+
+    });
+    freelancer.addEventListener('click', () => {
+
+        
+    });
+});
